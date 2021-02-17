c0466d489f89dcc17e21b0341d77f18a27562513
Angel Baro <angelbaro44@gmail.com>
2021-02-16 20:18:43 -0500

    Added some notes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let validParentheses = (str) => {
+      //First turn the string into an array
       let arr1=str.split('');
       console.log(`Init arr: ${arr1.join('')}`);
     
      for(let i =0;i<str.length;i++) {
-  
+    
+    // We will only check for '(' at [0] since 
+    // anything else would be false.
     if(arr1[0]==='('){
-
+      
+    // Now we check for and remove the pair. 
+    // If there is no ')' to match the '(' then we 
+    // override the array with ) resuting in false result.
         arr1.splice([0],1);
         arr1.includes(')')? arr1.splice( [ arr1.indexOf(')') ] ,1):arr1=[')'];
           }
